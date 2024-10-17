f7ebf0c6f48b058d1d79e1a0d36f4911b67dbca2
rahulgnw <rahul.g@nativewit.in>
2024-10-17 16:41:41 +0530

    Code Refactoring registration page.

--- chatify/lib/pages/register_page.dart
+++ chatify/lib/pages/register_page.dart
@@ -1,54 +1,148 @@
 import 'package:flutter/material.dart';
 
-class RegisterPage extends StatefulWidget{
+import '../widgets/app_text_form_field.dart';
+
+class RegisterPage extends StatefulWidget {
+  const RegisterPage({super.key});
+
   @override
   State<StatefulWidget> createState() {
-   return _RegisterPageState();
+    return _RegisterPageState();
   }
 }
-class _RegisterPageState extends State<RegisterPage>{
+
+class _RegisterPageState extends State<RegisterPage> {
   double? _deviceHeight, _deviceWidth;
+  String? _user;
+  String? _password;
+  String? _confirmpassword;
+  final GlobalKey<FormState> _registerFormKey = GlobalKey<FormState>();
+
   @override
   Widget build(BuildContext context) {
     _deviceHeight = MediaQuery.of(context).size.height;
     _deviceWidth = MediaQuery.of(context).size.width;
-   return Scaffold(
-
-     body: SafeArea(child: Container(
-       padding:EdgeInsets.symmetric(
-         horizontal: _deviceWidth!*0.05,
-       ),
-       child: Center(
-         child: Column(
-           mainAxisAlignment: MainAxisAlignment.spaceAround,
-           mainAxisSize: MainAxisSize.max,
-           crossAxisAlignment: CrossAxisAlignment.center,
-           children: [
-             _titleWidget()
-           ],
-         ),
-       ),
-     ),
-     )
-   );
+    return Scaffold(
+        body: SafeArea(
+      child: Container(
+        padding: EdgeInsets.symmetric(
+          horizontal: _deviceWidth! * 0.05,
+        ),
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            mainAxisSize: MainAxisSize.max,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              _titleWidget(),
+              _registrationForm(),
+              _registerbutton(),
+              _registerPageLink()
+            ],
+          ),
+        ),
+      ),
+    ));
   }
-  Widget _titleWidget(){
+
+  Widget _titleWidget() {
     return const Text(
       "Chatify",
-      style: TextStyle(
-          fontSize: 25,
-          fontWeight: FontWeight.w600),
+      style: TextStyle(fontSize: 25, fontWeight: FontWeight.w600),
     );
   }
-  widget _registerbutton(){
-    return MaterialButton(
-        onPressed:(){},
-    minWidth: _deviceWidth!*0.50,
-      height: _deviceHeight!*0.05,
-      color: Colors.white,
-      child: const Text(
-        c
+
+  Widget _registrationForm() {
+    return Container(
+      height: _deviceHeight! * 0.50,
+      child: Form(
+        key: _registerFormKey,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          mainAxisSize: MainAxisSize.max,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            _nameTextField(),
+            _passwordTextField (),
+            _confirmPassword()
+          ],
+        ),
       ),
+    );
+
+  }
+  Widget _nameTextField () {
+    return AppTextFormField(
+      hintText: "Enter username",
+      onSaved: (_value) {
+        setState(() {
+          _user=_value;
+        });
+      },
+      validator: (_value) {
+        bool _result = _value!.contains(
+            RegExp(r"^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$"));
+        return _result ? null : "Please enter a valid email";
+      },
+    );
+  }
+  Widget _passwordTextField () {
+    return AppTextFormField(
+      hintText: "Enter password",
+      onSaved: (_value) {
+        setState(() {
+          _password=_value;
+        });
+      },
+      validator: (_value) {
+        bool _result = _value!.contains(
+            RegExp(r"^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$"));
+        return _result ? null : "Please enter a valid email";
+      },
+    );
+  }
+  Widget _confirmPassword() {
+    return AppTextFormField(
+      hintText: "Enter confirm password",
+      onSaved: (_value) {
+        setState(() {
+          _confirmpassword=_value;
+        });
+      },
+      validator: (_value) {
+        bool _result = _value!.contains(
+            RegExp(r"^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$"));
+        return _result ? null : "Please enter a valid email";
+      },
+    );
+  }
+
+
+  Widget _registerbutton() {
+    return MaterialButton(
+      onPressed: () {},
+      minWidth: _deviceWidth! * 0.50,
+      height: _deviceHeight! * 0.05,
+      color: Colors.blue,
+      child: const Text(
+        'Register',
+        style: TextStyle(
+          fontSize: 25,
+          fontWeight: FontWeight.w400,
+        ),
+      ),
+    );
+  }
+
+  Widget _registerPageLink() {
+    return GestureDetector(
+      onTap: () => Navigator.pushNamed(context, 'Login'),
+      child: const Text("Already have an account?",
+          style: TextStyle(
+            color: Colors.blue,
+            fontSize: 25,
+            fontWeight: FontWeight.w600,
+          )),
     );
   }
 }
--- chatify/lib/widgets/app_text_form_field.dart
+++ chatify/lib/widgets/app_text_form_field.dart
@@ -6,22 +6,25 @@ class AppTextFormField extends StatelessWidget {
     required this.hintText,
     this.onSaved,
     this.validator,
+     this.obsecure=false,
   });
 
   final String hintText;
   final void Function(String?)? onSaved;
   final String? Function(String?)? validator;
+  final bool obsecure;
 
   @override
   Widget build(BuildContext context) {
     return TextFormField(
+      obscureText: obsecure,
       decoration: InputDecoration(
         hintText: hintText,
         filled: true,
-        fillColor: Colors.red.withOpacity(0.2),
+        fillColor: Colors.lightBlueAccent.withOpacity(0.2),
         border: OutlineInputBorder(
           borderRadius: BorderRadius.circular(8),
-          borderSide: BorderSide(color: Colors.red),
+          borderSide: BorderSide(color: Colors.lightBlueAccent),
         ),
       ),
       onSaved: onSaved,
